2622aeb2b570c1024b743111345b1eced9b5469a
Aswin Muraleedharan <aswin@Aswins-MacBook-Pro.local>
2020-12-17 22:09:47 +0530

    credentials

---
+++ credentials.py
@@ -0,0 +1,4 @@
+Username = 'fabthmqclfowrg'
+Password = '9093c3844925862efb4b888f95d8712517a121aa6c9eef526969d5eb7496d4a9'
+Database = 'ec2-54-234-44-238.compute-1.amazonaws.com:5432'
+Host = 'd66prq7bppuos4'
